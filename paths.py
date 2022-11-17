ec17fcb2f89793456050341e0558ba78339070e4
admonguadalest <pknrp2h8>
2022-11-17 19:37:04 +0100

    d

--- paths.py
+++ paths.py
@@ -1,6 +1,6 @@
 #central point for paths
 
-fashionmnist_dataset = "C:/Users/Carlos Conti/Desktop/Asuntos Corrientes Personales/AI/Popular Datasets/Fashion MNIST/"
-ocrmnist_dataset = "C:/Users/Carlos Conti/Desktop/Asuntos Corrientes Personales/AI/Popular Datasets/OCR MNIST/"
+fashionmnist_dataset = "C:/Popular Datasets/Fashion MNIST/"
+ocrmnist_dataset = "C:/Popular Datasets/OCR MNIST/"
 
-iris_dataset = "C:/Users/Carlos Conti/Desktop/Asuntos Corrientes Personales/AI/Popular Datasets/Iris Dataset"
+iris_dataset = "C:/Popular Datasets/Iris Dataset"
